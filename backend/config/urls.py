6cc35deea9889df1f0c869d7363ed60376e4a098
Taras Yurchyshyn <yts.prof@gmail.com>
2025-08-23 20:04:53 +0300

    add

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     path('select2/', include('django_select2.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -8,3 +8,10 @@ else:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+from django.conf import settings
+from whitenoise import WhiteNoise
+
+# WhiteNoise віддає і static, і media (як окремий префікс)
+application = WhiteNoise(application)
+application.add_files(settings.MEDIA_ROOT, prefix='media/')
