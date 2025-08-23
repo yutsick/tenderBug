0183aeaba91a65a8deb218278cfd2bf1833f6f23
Taras Yurchyshyn <yts.prof@gmail.com>
2025-08-23 19:15:09 +0300

    minor

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -215,11 +215,11 @@ SITE_ID = 1
 # CORS / CSRF (production)
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    "https://zahnbug-production.up.railway.app/",   # ← справжній фронтовий домен
+    "https://zahnbug-production.up.railway.app",   # ← справжній фронтовий домен
     "http://localhost:3000",              # якщо тестуєш локально
     "http://127.0.0.1:3000",
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "https://zahnbug-production.up.railway.app/",
+    "https://zahnbug-production.up.railway.app",
     "https://*.railway.app",              # довіряємо railway для форм/логіну
 ]
