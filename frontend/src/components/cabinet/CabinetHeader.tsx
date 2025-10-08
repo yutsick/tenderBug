9f024638bb66e8d0f0da8c5033ae333febe8a481
Taras Yurchyshyn <yts.prof@gmail.com>
2025-10-08 10:43:01 +0300

    minor

--- frontend/src/components/cabinet/CabinetHeader.tsx
+++ frontend/src/components/cabinet/CabinetHeader.tsx
@@ -6,11 +6,11 @@ export default function CabinetHeader({ user }: CabinetHeaderProps) {
   return (
     <div className="bg-white p-6 rounded-lg shadow-sm border border-gray-200">
       <div className="flex items-center gap-4 mb-4">
-        <div className="w-12 h-12 bg-green-600 text-white rounded-lg flex items-center justify-center font-bold text-xl">
-          ЗБ
+        <div className="w-8 h-8 ">
+          <img src="favicon-128.svg" alt="" />
         </div>
         <div>
-          <h3 className="text-lg font-semibold text-gray-700 mb-0">
+          <h3 className="text-2xl font-semibold text-[#2c6c20] mb-0">
             ЗАХІДНИЙ БУГ
           </h3>
         </div>
@@ -20,21 +20,21 @@ export default function CabinetHeader({ user }: CabinetHeaderProps) {
           </select>
         </div> */}
       </div>
-      
+
       <h2 className="text-2xl font-bold text-gray-800 mb-2">
         Вітаємо в кабінеті підрядника!
       </h2>
-      
+
       <p className="text-gray-600 text-lg mb-4">
         Вас було допущено до внесення інформації з ОП для кваліфікації учасника.
       </p>
-      
+
       <div className="mt-4">
         <p className="text-gray-700 font-medium mb-2">
 
           Підрозділ ПП «Західний Буг»: {user?.department_name || 'Не вказано'}
         </p>
-        
+
       </div>
     </div>
   );
